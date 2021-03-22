34cb40d2ef8eb42827f3566b4e40a34a8625270a
edwina <ta517152139@gmail.com>
2021-03-22 16:44:22 +0800

    #使用databinding的方法！！！

--- app/src/main/java/com/example/a20210322aboutme/MainActivity.kt
+++ app/src/main/java/com/example/a20210322aboutme/MainActivity.kt
@@ -8,54 +8,65 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.a20210322aboutme.databinding.ActivityMainBinding
 
 // Data Binding 可以取代 findViewById (省時間) //
+// 用法： binding.(id_name) //
+// DataBinding中 ‘id_name’ 的第二個字要大寫! //
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding  //ActivityMainBinding表示跟隨哪個layout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        //DataBinding的必要條件 (相對於 setContentView)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //DataBinding的必要條件 (相對於 setContentView)
+
+
+        binding.doneButton.setOnClickListener {
             addNickName(it)         //it 代表 button
         }
 
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+        binding.nicknameText.setOnClickListener {
             updateNickname(it)     //it 代表 當前輸入的nickname
         }
 
     }
 
-    private fun addNickName(view: View)         //view 代表 button
+    private fun addNickName(view: View)                          //view 代表 button
     {
-        val editText: EditText = findViewById(R.id.nickname_edit)           //way1
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)   //way2
-
-        nicknameTextView.text = editText.text           //顯示輸入的內容
-        editText.visibility = View.GONE                 //隱藏輸入欄
-        view.visibility = View.GONE                     //隱藏button
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {                                         //節省打很多binding
+            nicknameText.text = binding.nicknameEdit.text       //顯示輸入的內容
+            nicknameEdit.visibility = View.GONE                 //隱藏輸入欄
+            doneButton.visibility = View.GONE                   //隱藏button
+            nicknameText.visibility = View.VISIBLE
+        }
 
         //隱藏鍵盤
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 
     }
 
-    private fun updateNickname (view: View)     //view 代表 當前輸入的nickname
+    private fun updateNickname(view: View)                      //view 代表 當前輸入的nickname
     {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val doneButton = findViewById<Button>(R.id.done_button)
+        binding.apply {                                         //節省打很多binding
+            nicknameEdit.visibility = View.VISIBLE
+            doneButton.visibility = View.VISIBLE
+            nicknameText.visibility = View.GONE
 
-        editText.visibility = View.VISIBLE
-        doneButton.visibility = View.VISIBLE
-        view.visibility = View.GONE
+            // Set the focus to the edit text.
+            binding.nicknameEdit.requestFocus()
+        }
 
-        // Set the focus to the edit text.
-        editText.requestFocus()
         //出現鍵盤
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(editText, 0)
+        imm.showSoftInput(binding.nicknameEdit, 0)
 
     }
 
